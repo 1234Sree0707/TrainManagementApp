e9256a42cfbf3577ca044d273ebbdfbbc298f0fe
1234Sree0707 <sreerammpm@gmail.com>
2026-03-10 14:15:19 +0530

    Passenger bogie added

--- TrainManagement/src/com/seveneleven/trainmanagementapp/app/TrainApp.java
+++ TrainManagement/src/com/seveneleven/trainmanagementapp/app/TrainApp.java
@@ -4,17 +4,16 @@ import java.util.*;
 /**
  * MAIN CLASS
  * 
- * Use Case 1 : Initialize Train and Display Consist Summary
+ * Use Case 2 : Add Passenger Bogies to train
  * 
  * Description:
- * This class represents the entry point of the Train Consist 
- * Management Application.
+ * This class demonstrates how passenger bogies can be 
+ * managed dynamically using ArrayList operations. 
  * 
- * This use case introduces collection initialization and 
- * basic program startup flow.
+ * This use case maps CRUD operations using ArrayList.
  * 
  * @author Developer
- * @version 1.0
+ * @version 2.0
  * 
  */
 public class TrainApp{
@@ -25,14 +24,37 @@ public class TrainApp{
 		System.out.println("================================");
 		
 		List<String> trainConsist = new ArrayList<>();
-		System.out.println("Train Initialized Successfully.");
+		List<String> passengerBogies=new ArrayList<>();
+		passengerBogies.add("Sleeper");
+		passengerBogies.add("First class");
+		passengerBogies.add("AC Chair");
+		System.out.println("The train has "+passengerBogies.size()+"different types of bogies and they are:");
+		for(String bogie:passengerBogies) {
+			System.out.println(bogie);
+		}
+		System.out.println("=================================\n");
+
+		passengerBogies.remove("Sleeper");
+		System.out.println("Sleeper bogie removed");
+		System.out.println("=================================\n");
+
+		if(passengerBogies.contains("AC Chair")) {
+			System.out.println("AC Chair exisits");
+		}
+		System.out.println("=================================\n");
+		System.out.println("Updated bogie list:");
+		for(String bogie:passengerBogies) {
+			System.out.println(bogie);
+		}
+		System.out.println("=================================\n");
+
+		System.out.println("UC2 operations completed successfully");
 		
-		System.out.println("Initial Bogie Count : " + trainConsist.size());
-		System.out.println("Current Train Consist : " + trainConsist);
-		System.out.println();
-		System.out.println("System ready for operations...");
+		
+		
 
 		
 	}
+	
 
 }
