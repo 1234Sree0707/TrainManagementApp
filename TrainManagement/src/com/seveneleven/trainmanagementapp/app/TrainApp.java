58e9598235225665570d5a519a884f4c95111b3c
1234Sree0707 <sreerammpm@gmail.com>
2026-03-12 12:01:13 +0530

    Validate Train ID and cargo code

--- TrainManagement/src/com/seveneleven/trainmanagementapp/app/TrainApp.java
+++ TrainManagement/src/com/seveneleven/trainmanagementapp/app/TrainApp.java
@@ -1,24 +1,19 @@
 package com.seveneleven.trainmanagementapp.app;
-<<<<<<< HEAD
 import java.util.*;
 import java.util.stream.*;
 
-=======
->>>>>>> 5b41de1d7b1f1e144e665f55648a21e29e0b12a3
 /**
  * MAIN CLASS
  * 
- * Use Case 10 : Count total seats in Train
+ * Use Case 11 : Validate Train ID and Cargo Code
  * 
  * Description:
- * This class aggregates seating capacity of all bogies in a single total
- * using Stream reduce().
+ * This class validates input formats using Regular Expressions
  * 
- * This maps aggregation logic using reduce().
+ * This maps format validation logic using Pattern matching.
  * 
  * @author Developer
-<<<<<<< HEAD
- * @version 10.0
+ * @version 11.0
  * 
  */
 public class TrainApp {
@@ -31,13 +26,6 @@ public class TrainApp {
 			this.name=name;
 			this.capacity=capacity;
 		}
-=======
- * @version 9.0
-*/
- 
- import java.util.*;
- import java.util.stream.Collectors;
->>>>>>> 5b41de1d7b1f1e144e665f55648a21e29e0b12a3
 
 		public String getName() {
 			return name;
@@ -51,65 +39,30 @@ public class TrainApp {
 	
 
 	public static void main(String[] args) {
-		System.out.println("======================================");
-		System.out.println("==UC 10 : Count total seats in Train==");
-		System.out.println("======================================");
+		Scanner sc=new Scanner(System.in);
 		
-		List<Bogie> bogies=new ArrayList<>();
+		System.out.println("============================================");
+		System.out.println("Validate Train ID and Cargo Code==");
+		System.out.println("============================================");		
+		String trainno_regex="^TRN-\\d{4}$";
+		String cargono_regex="^PET-[A-Z]{2}$";
+		System.out.println("Enter Train ID (Format TRN-1234) :  ");
+		String trainno=sc.nextLine();
+		System.out.println("Enter Cargo Code (Format PET-AB) :  ");
+		String cargono=sc.nextLine();
+		System.out.println("");
+		System.out.println("Validation Results : ");
+		System.out.println("Train ID Valid : " + trainno.matches(trainno_regex));
+		System.out.println("Cargo Code Valid : " + cargono.matches(cargono_regex));
 		
-		System.out.println();
 		
-		bogies.add(new Bogie("First class",30));
-		bogies.add(new Bogie("Cargo",100));
-		bogies.add(new Bogie("Sleeper",45));
-		bogies.add(new Bogie("AC Chair",28));
-		bogies.add(new Bogie("AC Chair",15));
-		bogies.add(new Bogie("Cargo",76));
 		
-		System.out.println("All Bogies : ");
-		for(Bogie b:bogies) {
-			System.out.println( b.getName() + " -> " + b.getCapacity());
-		}
-		System.out.println();
 		
-		int total = bogies.stream().map(b -> b.getCapacity()).reduce(0,Integer::sum);
 		
-		System.out.println("Total seating capacity of Train : " + total);
+		
+		
+
+		
 	}
 
-<<<<<<< HEAD
 }
-=======
-         @Override
-         public String toString() {
-             return name + "->" + capacity;
-         }
-     }
-
-     public static void main(String[] args) {
-         List<Bogie> bogies = new ArrayList<>();
-         bogies.add(new Bogie("First Class", 24));
-         bogies.add(new Bogie("Cargo", 120));
-         bogies.add(new Bogie("Sleeper", 72));
-         bogies.add(new Bogie("AC Chair", 56));
-         bogies.add(new Bogie("AC Chair", 60));
-
-         System.out.println("======================================");
-         System.out.println("Grouping by types ");
-         System.out.println("======================================");
-         System.out.println("All Bogies:");
-         for (Bogie b : bogies) {
-             System.out.println(b.name + "->" + b.capacity);
-         }
-
-         Map<String, List<Bogie>> groupedBogies = bogies.stream()
-                 .collect(Collectors.groupingBy(b -> b.name));
-
-         System.out.println("Grouped bogies:");
-         for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
-             System.out.println(entry.getKey() + " -> " + entry.getValue());
-         }
-     }
- }
-
->>>>>>> 5b41de1d7b1f1e144e665f55648a21e29e0b12a3
